a31c8c5d97a38733aadb173996d786de6f388684
N . Sovna Nayak <164038409+Sovna-Nayak@users.noreply.github.com>
2025-01-30 00:24:17 +0530

    Add files via upload

---
+++ prg4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float miles;
+    printf("enter the miles");
+    scanf("%f",&miles);
+    float kilometers=miles*1.6;
+    printf("kilometers: %f",kilometers);
+    return 0;
+
+}
